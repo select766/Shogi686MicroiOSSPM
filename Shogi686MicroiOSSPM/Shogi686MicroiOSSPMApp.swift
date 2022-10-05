0fc7ea21e48737259a3a5c29651deb924c3c408a
select766 <select766@outlook.jp>
2022-10-05 16:12:02 +0900

    Initial Commit

---
+++ Shogi686MicroiOSSPM/Shogi686MicroiOSSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shogi686MicroiOSSPMApp.swift
+//  Shogi686MicroiOSSPM
+//
+//  Created by Masatoshi Hidaka on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct Shogi686MicroiOSSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
